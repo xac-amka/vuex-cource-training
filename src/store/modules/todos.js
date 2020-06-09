cee2db0180dc392ed65438b885aafde974faee1b
Proboss <64521451+xac-amka@users.noreply.github.com>
2020-06-09 14:11:33 +0800

    Add files via upload
    first commit :tomcat:

---
+++ src/store/modules/todos.js
@@ -0,0 +1,70 @@
+// create state, mutations, actions here
+import axios from 'axios';
+
+const state = {
+    todos: [
+        /* {
+             id: 1,
+             title: "Title one"
+         },{
+             id: 2,
+             title: "Title two"
+         } */
+    ]
+};
+
+const getters = {
+    allTodos: (state) => state.todos
+};
+
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        
+        commit('setTodos', response.data);
+    },
+    async addTodo({ commit }, title) {
+        const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false});
+        
+        commit('newTodo', response.data);
+    },
+    async deleteTodo({ commit }, id) {
+        // This remove from backend
+        await axios.delete('https://jsonplaceholder.typicode.com/todos/'+ id);
+        
+        commit('removeTodo', id);
+    },
+    async filterTodos({ commit }, e){
+        // Get selected number
+        const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+        console.log(limit)
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=' + limit);
+        console.log(response.data)
+        commit('setTodos', response.data);
+    },
+    async updateTodo({ commit }, updTodo) {
+        const response = await axios.put('https://jsonplaceholder.typicode.com/todos/' + updTodo.id, updTodo);
+
+        commit('updateTodo', response.data)
+    }
+};
+
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos),
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    // This remove from UI
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updTodo.id);
+        if(index !== -1) {
+            state.todos.splice(index, 1, updTodo);
+        }
+    }
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
